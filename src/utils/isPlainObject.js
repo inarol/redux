0d73c3a17e5ea7aec923420e96739563b65e8ebf
narol <junejinying@gmail.com>
2019-05-10 20:06:25 +0800

    Update isPlainObject.js

--- src/utils/isPlainObject.js
+++ src/utils/isPlainObject.js
@@ -1,6 +1,5 @@
 /**
  * 检测是否是纯粹的对象
- * while循环是解决objec可能来自其他
  * @param {any} obj The object to inspect.
  * @returns {boolean} True if the argument appears to be a plain object.
  */
@@ -8,9 +7,11 @@ export default function isPlainObject(obj) {
   if (typeof obj !== 'object' || obj === null) return false
 
   let proto = obj
+  // while循环是解决object可能来自其他环境
   while (Object.getPrototypeOf(proto) !== null) {
     proto = Object.getPrototypeOf(proto)
   }
-
+  // 简单实现可以用，Object.getPrototypeOf(proto) === Object.prototype
+  // Ojbect.getPrototypeOf(proto)相当于proto.__proto
   return Object.getPrototypeOf(obj) === proto
 }
